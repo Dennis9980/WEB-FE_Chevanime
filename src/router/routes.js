b1ef1ccfa11111c2b7f329425bb01ca1a078a653
Dennis9980 <dennis10.havinanda@gmail.com>
2022-05-24 14:41:21 +0700

    add firebase authentication

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,26 +7,31 @@ const routes = [
             {
                 path: '/',
                 name: 'TopAnime',
+                meta: { requiresAuth: false },
                 component: () => import('../views/TopAnime.vue')
             },
             {
                 path: '/top-manga',
                 name: 'TopManga',
+                meta: { requiresAuth: false },
                 component: () => import('../views/TopManga.vue')
             },
             {
                 path: '/schedule',
                 name: 'Schedule',
+                meta: { requiresAuth: false },
                 component: () => import('../views/ScheduleAnime.vue')
             },
             {
                 path: '/forum',
                 name: 'Forum',
+                meta: { requiresAuth: true },
                 component: () => import('../views/forum.vue')
             },
             {
                 path: '/forum/:id',
                 name: 'ForumDetail',
+                meta: { requiresAuth: true },
                 component: () => import('../views/forumDetail.vue')
             }
         ]
@@ -34,11 +39,13 @@ const routes = [
     {
         path: '/login',
         name: 'Login',
+        meta: { requiresAuth: false },
         component: () => import ('../views/auth/login.vue')
     },
     {
         path: '/register',
         name: 'Register',
+        meta: { requiresAuth: false },
         component: () => import ('../views/auth/register.vue')
     }
 ];
